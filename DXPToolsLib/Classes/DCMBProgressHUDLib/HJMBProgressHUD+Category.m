f9e8255c283d8c9c55b30e433dfe8c0b6f0e550c
李标 <li.biao3@iwhalecloud.com>
2025-10-28 12:17:08 +0800

    新增项目判断

--- DXPToolsLib/Classes/DCMBProgressHUDLib/HJMBProgressHUD+Category.m
+++ DXPToolsLib/Classes/DCMBProgressHUDLib/HJMBProgressHUD+Category.m
@@ -15,7 +15,7 @@
 + (instancetype)showLoading {
     
     NSString *appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
-    if ([appName isEqualToString:@"POST"] || [appName isEqualToString:@"ETA"]) {
+    if ([appName isEqualToString:@"POST"] || [appName isEqualToString:@"ETA"] || [appName isEqualToString:@"ELH"]) {
         // TM
         NSString *path = [[NSBundle mainBundle] pathForResource:@"CLP_Loading" ofType:@"gif"];
         NSData *gifData = [NSData dataWithContentsOfFile:path];
